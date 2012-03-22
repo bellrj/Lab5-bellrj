3ac2af5bc4081db5d94f4ab8b9087d302eaa383f
Ryne Bell <bellrj@rose-hulman.edu>
2012-03-22 19:35:53 -0400

    passes test for 8

--- PrimeFactorization/src/PrimeFactorization.java
+++ PrimeFactorization/src/PrimeFactorization.java
@@ -8,7 +8,7 @@ public class PrimeFactorization{
 	public static ArrayList<Integer> getPrimeFactors(int n){
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
-		if(n%2 == 0)
+		while(n%2 == 0)
 		{
 			list.add(2);
 			n /= 2;
