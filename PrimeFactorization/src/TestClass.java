de303c1b600f5470088ee515d0e7f76cc17a21b4
Ryne Bell <bellrj@rose-hulman.edu>
2012-03-22 19:27:28 -0400

    added second test

--- PrimeFactorization/src/TestClass.java
+++ PrimeFactorization/src/TestClass.java
@@ -14,6 +14,11 @@ public class TestClass  extends TestCase{
 		assertEquals(list(),PrimeFactorization.getPrimeFactors(1));
 	}
 	
+	@Test
+	public void testTwo(){
+		assertEquals(list(2), PrimeFactorization.getPrimeFactors(2));
+	}
+	
 	public ArrayList<Integer> list(int... ints){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for(int i : ints){
