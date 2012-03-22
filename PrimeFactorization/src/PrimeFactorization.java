6d1587e1d289c87b43d0e2d323c91c33ff386e89
Ryne Bell <bellrj@rose-hulman.edu>
2012-03-22 19:29:20 -0400

    passed second test

--- PrimeFactorization/src/PrimeFactorization.java
+++ PrimeFactorization/src/PrimeFactorization.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 public class PrimeFactorization{
 	
 	public static ArrayList<Integer> getPrimeFactors(int n){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if(n == 2)
+		{
+			list.add(2);
+		}
+		return list;
 	}
 
 }
