9d115568545120a9eba704a85cf7d2141cadf492
Ryne Bell <bellrj@rose-hulman.edu>
2012-03-22 20:01:05 -0400

    passed 5 lesserPrimes test

--- PrimeFactorization/src/PrimeFactorization.java
+++ PrimeFactorization/src/PrimeFactorization.java
@@ -1,17 +1,12 @@
 import java.util.ArrayList;
 
+public class PrimeFactorization {
 
-
-
-public class PrimeFactorization{
-	
-	public static ArrayList<Integer> getPrimeFactors(int n){
+	public static ArrayList<Integer> getPrimeFactors(int n) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		for(int num = 2; n > 1;num++)
-		{
-			while(n%num == 0)
-			{
+
+		for (int num = 2; n > 1; num++) {
+			while (n % num == 0) {
 				list.add(num);
 				n /= num;
 			}
@@ -21,9 +16,13 @@ public class PrimeFactorization{
 
 	public static ArrayList<Integer> getLesserPrimes(int i) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		for(int j=2;j<i;j++){
-			list.add(j);
+
+		for (int j = 2; j < i; j++) {
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			temp.add(j);
+			if (getPrimeFactors(j).equals(temp)) {
+				list.add(j);
+			}
 		}
 		return list;
 	}
